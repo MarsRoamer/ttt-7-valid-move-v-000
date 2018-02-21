96c65ad98ccca32a3ec63994f8fcf8a4b105420e
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-21 16:27:13 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,8 @@
-
+def valid_move?(board, index)
+  if index.between?(0,8)
+    true
+  end
+end
 
 def position_taken?(board, index)
   if(board[index] == " " || board[index] == "" || board[index] == nil)
