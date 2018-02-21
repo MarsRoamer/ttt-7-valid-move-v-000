85dad95405d320f6f0749f7c478888d572b55db2
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-21 16:18:11 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 def valid_move?(board, index)
-  if(index >= 0 && index < = 8) && (position_taken?(board, index) == false)
+  if(index.between?(0, 8)) && (position_taken?(board, index) == false)
 end
 
 def position_taken?(board, index)
